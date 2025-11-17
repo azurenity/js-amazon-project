ee7e934bae40f0f1a093607975e579a7bada6d72
Phua Guan Cen <gc@Phuas-MacBook-Air.local>
2025-11-17 16:51:07 +0800

    fixed the delete button at the checkout page to actually remove the product

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
       <div class="cart-item-details-grid">
@@ -33,15 +34,15 @@ cart.forEach((cartItem) => {
          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
-            <span> Quantity: <span class="quantity-label">${
-              cartItem.quantity
-            }</span> </span>
-            <span class="update-quantity-link link-primary">
+          <span> Quantity: <span class="quantity-label">${
+            cartItem.quantity
+          }</span> </span>
+          <span class="update-quantity-link link-primary">
             Update
-            </span>
-            <span class="delete-quantity-link link-primary">
+          </span>
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
-            </span>
+          </span>
         </div>
         </div>
 
@@ -88,6 +89,18 @@ cart.forEach((cartItem) => {
       </div>
   `;
 
-  document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
-  console.log(cartSummaryHTML);
+  document.querySelector(".js-order-summary")
+    .innerHTML = cartSummaryHTML;
+
+  document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+      link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
+        console.log(container);
+      })
+    })
 });
